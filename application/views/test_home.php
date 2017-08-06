9eb80614a50c6f4de8fe180645bc19f31c547b1b
orr <orr@orr-desktop>
2017-08-06 20:29:15 +0700

    Add test_home.php

--- application/views/test_home.php
+++ application/views/test_home.php
@@ -3,14 +3,19 @@
 Testing for orr-projects
 -->
 <html>
-    <meta charset="UTF-8">
-    <title>Testing for orr-projects</title>
-    <?php foreach ($css_files as $file): ?>
-        <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
-    <?php endforeach; ?>
-    <?php foreach ($js_files as $file): ?>
-        <script src="<?php echo $file; ?>"></script>
-    <?php endforeach; ?>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Testing for orr-projects</title>
+        <?php foreach ($css_files as $file): ?>
+            <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+        <?php endforeach; ?>
+        <?php foreach ($js_files as $file): ?>
+            <script src="<?php echo $file; ?>"></script>
+        <?php endforeach; ?>
+    </head>
     <body>
         <?php
         // put your code here
